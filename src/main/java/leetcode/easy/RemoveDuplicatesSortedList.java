ff7698364fc070513cf9050a69aade534d833ef3
Agustin Ignacio Rossi <agustin.ignacio.rossi@gmail.com>
2024-07-21 12:37:23 +0200

    Refactoring - Adding ListNode as common model
    Refactoring - Adding ListNode as common model

--- src/main/java/leetcode/easy/RemoveDuplicatesSortedList.java
+++ src/main/java/leetcode/easy/RemoveDuplicatesSortedList.java
@@ -1,47 +1,14 @@
 package leetcode.easy;
 
+import leetcode.models.ListNode;
+
 public class RemoveDuplicatesSortedList {
-    public static void main(String[] args) {
-        ListNode firstList = new ListNode(
-                1,
-                new ListNode(
-                        1,
-                        new ListNode(2)
-                )
-        );
 
-        ListNode firstExampleOutput = deleteDuplicates(firstList);
-        System.out.println("First example list is now: ");
-        while(firstExampleOutput != null) {
-            System.out.println(firstExampleOutput.val);
-            firstExampleOutput = firstExampleOutput.next;
-        }
-
-        ListNode secondList = new ListNode(
-                1,
-                new ListNode(
-                        1,
-                        new ListNode(
-                                2,
-                                new ListNode(
-                                        3,
-                                        new ListNode(3)
-                                )
-                        )
-                )
-        );
-
-        ListNode secondExampleOutput = deleteDuplicates(secondList);
-        System.out.println("Second example list is now: ");
-        while(secondExampleOutput != null) {
-            System.out.println(secondExampleOutput.val);
-            secondExampleOutput = secondExampleOutput.next;
-        }
-    }
+    public RemoveDuplicatesSortedList(){}
 
     // Time Complexity: O(n)
     // Space Complexity: O(1)
-    private static ListNode deleteDuplicates(ListNode head) {
+    public ListNode deleteDuplicates(ListNode head) {
         if(head == null)
             return null;
 
@@ -50,17 +17,7 @@ public class RemoveDuplicatesSortedList {
             if(head.val == head.next.val)
                 head.next = head.next.next;
             else head = head.next;
-
         }
         return startingHead;
     }
-
-    public static class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
 }
---
+++ src/test/java/leetcode/easy/RemoveDuplicatesSortedListTest.java
@@ -0,0 +1,62 @@
+package leetcode.easy;
+
+import leetcode.models.ListNode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class RemoveDuplicatesSortedListTest {
+
+    private RemoveDuplicatesSortedList underTest;
+
+    @BeforeEach
+    void setup() {
+        underTest = new RemoveDuplicatesSortedList();
+    }
+
+    @Test
+    void firstTest(){
+        ListNode firstList = new ListNode(
+                1,
+                new ListNode(
+                        1,
+                        new ListNode(2)
+                )
+        );
+        ListNode response = underTest.deleteDuplicates(firstList);
+        int size = 0;
+        while (response != null) {
+            size++;
+            response = response.next;
+        }
+        assertEquals(2, size);
+    }
+
+    @Test
+    void secondTest(){
+        ListNode secondList = new ListNode(
+                1,
+                new ListNode(
+                        1,
+                        new ListNode(
+                                2,
+                                new ListNode(
+                                        3,
+                                        new ListNode(3)
+                                )
+                        )
+                )
+        );
+        ListNode response = underTest.deleteDuplicates(secondList);
+        int size = 0;
+        while (response != null) {
+            size++;
+            response = response.next;
+        }
+        assertEquals(3, size);
+    }
+}
